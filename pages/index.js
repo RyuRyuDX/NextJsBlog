5cbef160f6c499534dc216a67152222e8ddf8910
Ryusuke Onose <gibson.red.gin.suke@gmail.com>
2021-10-10 00:13:17 +0900

    【ADD】追加

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import React from 'react';
+import { styled } from '@stitches/react';
+
 
 const Home = ({blogs}) => {
   return (
     <div>
-      <h2>最新の記事</h2>
+      <h2>記事一覧</h2>
+      <Button>Button</Button>
+
       <div>
         {blogs.map(blog => (
           <React.Fragment key={blog.id}>
@@ -41,5 +45,15 @@ export const getStaticProps = async () => {
     }
   }
 };
+
+const Button = styled('button', {
+  backgroundColor: 'red',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+});
 
 export default Home;
